595fb95d2f7e0ea54d0cb576aa7d40513d48ddb7
feng gang <fglovezzr@163.com>
2017-09-07 10:56:18 +0800

    初始化项目

---
+++ src/main/java/com/nuoxin/virtual/rep/virtualrepapi/VirtualRepApiApplication.java
@@ -0,0 +1,12 @@
+package com.nuoxin.virtual.rep.virtualrepapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualRepApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualRepApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nuoxin/virtual/rep/virtualrepapi/VirtualRepApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nuoxin.virtual.rep.virtualrepapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VirtualRepApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
