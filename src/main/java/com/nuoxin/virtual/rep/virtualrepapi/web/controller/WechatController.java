1859ae2fd8b1cb2cf570cd126aca07e91602dc44
tiancun <2716804233@qq.com>
2017-09-12 14:04:28 +0800

    微信消息实体

--- src/main/java/com/nuoxin/virtual/rep/virtualrepapi/web/controller/WechatController.java
+++ src/main/java/com/nuoxin/virtual/rep/virtualrepapi/web/controller/WechatController.java
@@ -1,12 +1,16 @@
 package com.nuoxin.virtual.rep.virtualrepapi.web.controller;
 
-import com.nuoxin.virtual.rep.virtualrepapi.common.bean.DefaultResponseBean;
 import com.nuoxin.virtual.rep.virtualrepapi.common.controller.BaseController;
 import com.nuoxin.virtual.rep.virtualrepapi.service.WechatService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Api(value = "微信相关接口")
 @RequestMapping(value = "/wechat")
@@ -17,7 +21,12 @@ public class WechatController extends BaseController{
     private WechatService wechatService;
 
 
+    @ApiOperation(value = "微信消息导入接口", notes = "微信消息导入接口")
+    @PostMapping("/importExcel")
+    public void importExcel(HttpServletRequest request, HttpServletResponse response){
 
+
+    }
 
 
 }
