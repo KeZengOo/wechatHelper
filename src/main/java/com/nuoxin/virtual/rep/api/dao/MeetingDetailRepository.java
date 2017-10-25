6a40dc34dede140f87b1d546453901721108c9d5
tiancun <2716804233@qq.com>
2017-10-25 21:21:11 +0800

    修复bug

--- src/main/java/com/nuoxin/virtual/rep/api/dao/MeetingDetailRepository.java
+++ src/main/java/com/nuoxin/virtual/rep/api/dao/MeetingDetailRepository.java
@@ -5,13 +5,18 @@ import com.nuoxin.virtual.rep.api.entity.MeetingDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 /**
  * Create by tiancun on 2017/10/11
  */
 public interface MeetingDetailRepository extends JpaRepository<MeetingDetail, Long>,JpaSpecificationExecutor<MeetingDetail> {
 
 
-    public void deleteAllByMeetingId(Long meetingId);
+   void deleteAllByMeetingId(Long meetingId);
 
+   List<MeetingDetail> findByMeetingIdAndDoctorId(Long meetingId, Long doctorId);
+
+   void deleteAllByMeetingIdAndDoctorId(Long meetingId, Long doctorId);
 
 }
