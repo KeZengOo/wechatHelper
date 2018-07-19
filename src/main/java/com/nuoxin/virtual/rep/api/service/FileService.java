1644f1f5e9bf96d676fb71a38d2274873aaf97ad
xiekaiyu <xiekaiyu@hujiang.com>
2018-07-19 14:29:58 +0800

    Fixes: 去掉无用的引入

--- src/main/java/com/nuoxin/virtual/rep/api/service/FileService.java
+++ src/main/java/com/nuoxin/virtual/rep/api/service/FileService.java
@@ -6,7 +6,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 
