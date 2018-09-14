69efea356d6f97a6c0171c14d56e282c0e95f136
xiekaiyu <xiekaiyu@hujiang.com>
2018-09-14 16:56:03 +0800

    Fixes: 修正

--- src/main/java/com/nuoxin/virtual/rep/api/web/controller/request/v2_5/doctor/SaveVirtualDoctorMendRequest.java
+++ src/main/java/com/nuoxin/virtual/rep/api/web/controller/request/v2_5/doctor/SaveVirtualDoctorMendRequest.java
@@ -10,9 +10,9 @@ public class SaveVirtualDoctorMendRequest {
 	@NotNull(message="productLineId is null")
 	@ApiModelProperty(value = "产品ID")
 	private Integer productLineId;
-	@ApiModelProperty(value = "是否有药")
+	@ApiModelProperty(value = "是否有药 1是,0否,-1未知")
 	private Integer isHasDrug;
-	@ApiModelProperty(value = "是否招募")
+	@ApiModelProperty(value = "是否招募 1是,0否,-1未知")
 	private Integer isRecruit;
 	@ApiModelProperty(value = "客户等级")
 	private String hcpLevel;
