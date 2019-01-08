29a4f4f992f37af2b9c05e2fb0bc3dc0856f8e04
tiancun <2716804233@qq.com>
2019-01-08 17:05:20 +0800

    修复bug

--- src/main/java/com/nuoxin/virtual/rep/api/web/controller/request/v2_5/set/HolidayRequestBean.java
+++ src/main/java/com/nuoxin/virtual/rep/api/web/controller/request/v2_5/set/HolidayRequestBean.java
@@ -1,6 +1,7 @@
 package com.nuoxin.virtual.rep.api.web.controller.request.v2_5.set;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -14,4 +15,14 @@ import java.io.Serializable;
 @ApiModel(value = "法定节假日请求参数")
 public class HolidayRequestBean implements Serializable {
     private static final long serialVersionUID = -5239755974912009463L;
+
+    @ApiModelProperty(value = "节假日名称")
+    private String name;
+
+    @ApiModelProperty(value = "开始日期，YYYY-MM-DD格式")
+    private String startDate;
+
+    @ApiModelProperty(value = "天数")
+    private Integer days;
+
 }
