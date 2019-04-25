ddc64f4d98f15de982eabcb432455fb2475d1956
tiancun <2716804233@qq.com>
2019-04-25 12:35:06 +0800

    添加Excel的工具类

---
+++ src/main/java/com/nuoxin/virtual/rep/api/utils/ExportExcelWrapper.java
@@ -0,0 +1,71 @@
+package com.nuoxin.virtual.rep.api.utils;
+
+import java.net.URLEncoder;
+import java.util.Collection;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * 包装类
+ * @author liuyazhuang
+ *
+ * @param <T>
+ */
+public class ExportExcelWrapper<T> extends ExportExcelUtil<T> {
+    /**
+     * <p>
+     * 导出带有头部标题行的Excel <br>
+     * 时间格式默认：yyyy-MM-dd hh:mm:ss <br>
+     * </p>
+     *
+     * @param title 表格标题
+     * @param headers 头部标题集合
+     * @param dataset 数据集合
+     * @param version 2003 或者 2007，不传时默认生成2003版本
+     */
+    public void exportExcel(String fileName, String title, String[] headers, Collection<T> dataset, HttpServletResponse response,String version) {
+        try {
+            response.setContentType("application/vnd.ms-excel");
+            response.addHeader("Content-Disposition", "attachment;filename="+ URLEncoder.encode(fileName, "UTF-8") + ".xls");
+            if(StringUtils.isBlank(version) || EXCEL_FILE_2003.equals(version.trim())){
+                exportExcel2003(title, headers, dataset, response.getOutputStream(), "yyyy-MM-dd hh:mm:ss");
+            }else{
+                exportExcel2007(title, headers, dataset, response.getOutputStream(), "yyyy-MM-dd hh:mm:ss");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * demo调用案例
+     *
+     *@Controller("test")
+     * @RequestMapping("/test")
+     * public class TestController {
+     * 	@RequestMapping("/get/excel")
+     * 	public void getExcel(HttpServletRequest request, HttpServletResponse response) throws Exception {
+     * 		// 准备数据
+     * 		List<Student> list = new ArrayList<>();
+     * 		for (int i = 0; i < 10; i++) {
+     * 			 list.add(new Student(111,"张三asdf","男"));
+     * 		     list.add(new Student(111,"李四asd","男"));
+     * 		     list.add(new Student(111,"王五","女"));
+     * 		}
+     * 		String[] columnNames = { "ID", "姓名", " 性别"};
+     * 		String fileName = "excel1";
+     * 		ExportExcelWrapper<Student> util = new ExportExcelWrapper<Student>();
+     * 		util.exportExcel(fileName, fileName, columnNames, list, response, ExportExcelUtil.EXCEL_FILE_2003);
+     * 	}
+     * }
+     *
+     *
+     *
+     *
+     */
+
+
+
+}
